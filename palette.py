9b5f227840a4c892d69884de323b60272b473fac
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-11-11 04:32:41 -0500

    setting up kwargs to use in command line

--- palette.py
+++ palette.py
@@ -4,8 +4,24 @@ from PIL import Image, ImageDraw
 img = Image.new('RGB', (500, 100), (255, 255, 255))
 draw = ImageDraw.Draw(img)
 
-draw.rectangle(
-   (0, 100, 100, 0),
-   fill=(255, 0, 0))
-img.show()
-img.save('output.png', "PNG");
+def generate_palette():
+    for x in range(5):
+        draw.rectangle((0, 0, (x+1)*100, (x+1)*100), fill=(255, 0, 0))
+    img.show()
+    img.save('output.png', "PNG");
+
+# generate_palette();
+
+def driver(mode, **kwargs):
+    if mode == 'hex':
+        print('hex mode');
+    elif mode == 'rgb':
+        print('RGB mode')
+    else:
+        print('not valid');
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('arguments needed')
+    else:
+        driver(sys.argv[1]);
