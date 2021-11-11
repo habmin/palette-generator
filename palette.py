ca6b8df0e48fe03ee44e037639f8ffa2da8c435b
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-11-11 18:21:13 -0500

    valid hex input

--- palette.py
+++ palette.py
@@ -1,6 +1,7 @@
 import sys
 from subprocess import run
 import argparse
+import re
 import importlib.util
 
 
@@ -15,37 +16,44 @@ import importlib.util
 
 # generate_palette();
 
-def driver(mode, **kwargs):
-    if mode == 'hex':
-        print('hex mode');
-    elif mode == 'rgb':
-        print('RGB mode')
-    else:
-        print('not valid');
+def validate_hex(string):
+    regex = re.compile('[0-9a-fA-F]{6}\Z', re.I)
+    print(str(string))
+    match = regex.match(str(string));
+    print(bool(match))
+    return bool(match)
 
-# if __name__ == '__main__':
-    # try:
-    #     from PIL import Image, ImageDraw
-    # except ModuleNotFoundError:
-    #     print("not found")
-    # if len(sys.argv) < 2:
-    #     print('arguments needed')
-    # else:
-    #     driver(sys.argv[1]);
+def color_convertor(color_string):
+    color_list = []
+    # check to see if the argument passed is hexadecimal value
+    if color_string[:1] == '#':
+        color_string = color_string[1:]
+        # check for valid hex input
+        if validate_hex(color_string):
+            sys.exit("Valid hex value");
+        else:
+            sys.exit("Invalid hex value, must be six digits long in rrbbgg form")
 
 
-parser = argparse.ArgumentParser(description='Color Palette Generator')
-subparsers = parser.add_subparsers(help='sub-command help')
-parser.add_argument('--palette', '-p', dest='palette', default='all', 
-                    choices=['a', 'analog', 'analogous',
-                             'm', 'mono', 'monochromatic',
-                             'c', 'comp', 'complimentary',
-                             'A', 'all'])
-parser.add_argument('--color', '-c', dest='color', default='#00FF00')
-parser.add_argument('--size', '-s', dest='size', type=int, default=5, choices=range(1, 11))
-parser.add_argument('--print', '-pr', dest='print', type=bool, default=True)
-parser.add_argument('--display', '-d', dest='display', type=bool, default=False)
-parser.add_argument('--output', '-o', dest='output', default=None)
 
-args = parser.parse_args()
-print(args.palette, args.color, args.size, args.print, args.display, args.output)
+def driver(*args, **kwargs):
+    parser = argparse.ArgumentParser(description='Color Palette Generator')
+    parser.add_argument('--palette', '-p', type=str, dest='palette', default='all', 
+                        choices=['a', 'analog', 'analogous',
+                                'm', 'mono', 'monochromatic',
+                                'c', 'comp', 'complimentary',
+                                'A', 'all'])
+    parser.add_argument('--color', '-c', dest='color', default='#00FF00')
+    parser.add_argument('--size', '-s', dest='size', type=int, default=5, choices=range(1, 11))
+    parser.add_argument('--print', '-pr', dest='print', type=bool, default=True)
+    parser.add_argument('--display', '-d', dest='display', type=bool, default=False)
+    parser.add_argument('--output', '-o', dest='output', default=None)
+
+    args = parser.parse_args()
+
+    color_convertor(args.color)
+    
+    #if (args.palette[:1] == 'a'):
+
+if __name__ == '__main__':
+    driver(*sys.argv)
