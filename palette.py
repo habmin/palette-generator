eef5db205f12b6b7d738da54e4f85a22b5aa1bac
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-11-12 15:20:04 -0500

    Display options for mono, comp, analog, and all palettes

--- palette.py
+++ palette.py
@@ -44,22 +44,19 @@ def create_analogous(color_list, size):
     middle = color_list
     
     palette_list = []
-    increment = (60/359) / ((size - 1) // 2)
+    increment = (60/359) / (size / 2)
 
     for i in range(size):
         if i == size // 2:
             palette_list.append(middle)
         else:
-            print(start_hsv[0] + (increment * i))
             rgb = hsv_to_rgb(start_hsv[0] + (increment * i), start_hsv[1], start_hsv[2])
-            print(rgb)
             palette_list.append([int(rgb[0] * 255), int(rgb[1] * 255), int(rgb[2] * 255)])
     
     return palette_list
 
 
 def display_palette(palette, size):
-    print("draw please")
     from PIL import Image, ImageDraw
     # create white canvas based on size
     img = Image.new('RGB', ((100 * size), 100), (255, 255, 255))
@@ -67,20 +64,13 @@ def display_palette(palette, size):
     for i in range(size):
         draw.rectangle(((100 * i), 0, (i + 1) * 100, (i + 1) * 100), fill=(palette[i][0], palette[i][1], palette[i][2]))
     img.show()
-    # else:
-    #     print("no pil")
-#     img.show()
-#     img.save('output.png', "PNG");
-# draw = ImageDraw.Draw(img)
 
 def validate_hex(string):
     regex = re.compile('[0-9a-fA-F]{6}\Z', re.I)
-    print(str(string))
-    match = regex.match(str(string));
-    print(bool(match))
+    match = regex.match(str(string))
     return bool(match)
 
-def color_convertor(color_string):
+def color_parser(color_string):
     return_color = []
     # check to see if the argument passed is hexadecimal value
     if color_string[:1] == '#':
@@ -93,8 +83,8 @@ def color_convertor(color_string):
         else:
             sys.exit("Invalid hex value, must be six digits long in rrbbgg form")
     else:
-        # split string by white space or comma
-        string_list = re.split('[ ,]+', color_string)
+        # split string by white space, comma, or parentheses
+        string_list = re.split('[() ,]+', color_string)
         # check to see if there are eactly three value
         if len(string_list) != 3:
             sys.exit('Must have only three RGB int values')
@@ -105,6 +95,13 @@ def color_convertor(color_string):
             else:
                 return_color.append(int(number))
     return return_color
+
+def hex_to_rgb(color_palette):
+    convert_palette = []
+    for cell in color_palette:
+        convert_cell = ''.join('{:02x}'.format(x) for x in cell)
+        convert_palette.append(f'#{convert_cell.upper()}')
+    return convert_palette
 
 def driver(*args, **kwargs):
     parser = argparse.ArgumentParser(description='Color Palette Generator')
@@ -122,27 +119,44 @@ def driver(*args, **kwargs):
 
     args = parser.parse_args()
 
-    color_list = color_convertor(args.color)
+    palette_choices = ["Monochromatic", "Complimentary", "Analogous"]
+
+    color_list = color_parser(args.color)
     
     color_palette = []
 
-    if (args.palette[:1] == 'a' and args.palette != 'all'):
-        color_palette = create_analogous(color_list, args.size)
+    if (args.palette[:1] == 'm'):
+        color_palette.append(create_monochromatic(color_list, args.size))
     elif (args.palette[:1] == 'c'):
-        color_palette = create_complimentary(color_list, args.size)
-    elif (args.palette[:1] == 'm'):
-        color_palette = create_monochromatic(color_list, args.size)
-    if args.display:
-        display_palette(color_palette, args.size)
+        color_palette.append(create_complimentary(color_list, args.size))
+    elif (args.palette[:1] == 'a' and args.palette != 'all'):
+        color_palette.append(create_analogous(color_list, args.size))
+    elif (args.palette[:1] == 'A' or args.palette == 'all'):
+        color_palette.append(create_monochromatic(color_list, args.size))
+        color_palette.append(create_complimentary(color_list, args.size))
+        color_palette.append(create_analogous(color_list, args.size))
 
-    if args.print == 'hex' or (args.color[:1] == '#' and args.print is None):
-        convert_palette= []
-        for cell in color_palette:
-            convert_cell = ''.join('{:02x}'.format(x) for x in cell)
-            convert_palette.append(f'#{convert_cell.upper()}')
-        print(convert_palette);
-    elif args.print == 'rgb' or args.print is None:
-        print(color_palette)
+    if args.display:
+        if args.palette[:1] == 'A' or args.palette == 'all':
+            for palette in color_palette:
+                display_palette(palette, args.size)
+        else:
+            display_palette(color_palette, args.size)
+
+    if args.print != 'none':
+        print("\n")
+        print_range = 3 if (args.palette == 'A' or args.palette == 'all') else 1
+        choice = 0 if (args.palette == args.palette[:1] == 'm') else 1 if args.palette[:1] == 'c' else 2 if (args.palette[:1] == 'a' and args.palette != 'all') else 0
+        for i in range(print_range):
+            print(f"****** {palette_choices[choice]} of {args.color} ******")
+            if args.print == 'hex' or (args.color[:1] == '#' and args.print is None):
+                convert_palette = hex_to_rgb(color_palette[i])
+                print(convert_palette);
+            else:
+                print(color_palette[i])
+            print("\n")
+            choice += 1        
+        # elif args.print == 'rgb' or args.print is None:
     # elif args.print == 'rgb' or args.color is None:
 
 
